843452cf5bfe3edbc2d20f03f65d1c15e8fa1963
soriag <soriag@shure.com>
2022-08-21 14:38:48 -0600

    challenge 5-10

--- 05_04/script.js
+++ 05_04/script.js
@@ -2,3 +2,6 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+console.log(document.querySelector
+    ( "li.backpack__volume"))
