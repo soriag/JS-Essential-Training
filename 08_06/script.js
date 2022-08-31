f06fc3b5ccabb057c009bbd7ffd9000802a025f5
soriag <soriag@shure.com>
2022-08-31 10:29:00 -0600

    kjda

--- 08_06/script.js
+++ 08_06/script.js
@@ -5,3 +5,36 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// function insertTitle (stringTitle) {
+//     const title = document.createElement("h1");
+
+//     title.innerHTML = `
+//         <h1>${stringTitle}</h1>
+//     `
+//     return title;
+// }
+
+// document.querySelector("main").append(insertTitle("I am a title"));
+
+// const insertTitle = function (stringTitle) {
+//     const title = document.createElement("h1");
+
+//     title.innerHTML = `
+//         <h1>${stringTitle}</h1>
+//     `
+//     return title;
+// }
+
+// document.querySelector("main").append(insertTitle("I am a title"));
+
+const insertTitle = (stringTitle) => {
+    const title = document.createElement("h1");
+
+    title.innerHTML = `
+        <h1>${stringTitle}</h1>
+    `
+    return title;
+}
+
+document.querySelector("main").append(insertTitle("I am a title"));
